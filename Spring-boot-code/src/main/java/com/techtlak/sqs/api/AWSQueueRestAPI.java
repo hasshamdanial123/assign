bc5c96e1b8ba57c71d8d2044cc9181b565defb41
hasshamdanial123 <61381520+hasshamdanial123@users.noreply.github.com>
2021-06-07 21:51:26 +0530

    Update AWSQueueRestAPI.java

--- Spring-boot-code/src/main/java/com/techtlak/sqs/api/AWSQueueRestAPI.java
+++ Spring-boot-code/src/main/java/com/techtlak/sqs/api/AWSQueueRestAPI.java
@@ -43,27 +43,6 @@ public class AWSQueueRestAPI {
 	public void getMessage(String message) {
 		LOG.info("Message from Received from AWS SQS Queue - " + message);
 	}
-	@Autowired
-        private MockMvc mvc;
- 
-        @Test
-        public void sendMessageTest() throws Exception 
-            {
-             mvc.perform( MockMvcRequestBuilders
-             .get("/v1/api/sqs")
-             .accept(MediaType.APPLICATION_JSON))
-             .andDo(print())
-             .andExpect(status().isOk())
-            }
-	@Test
-        public void PostMessageTest() throws Exception 
-          {
-           mvc.perform( MockMvcRequestBuilders
-             .post("/v1/api/sqs")
-             .content(asJsonString("Testing POST request"))
-             .contentType(MediaType.APPLICATION_JSON)
-             .accept(MediaType.APPLICATION_JSON))
-             .andExpect(status().isCreated())
-      }
+
 	
 }
